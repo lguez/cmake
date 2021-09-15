0131da99f31d3589d7d43440c7737349d1ed3437
Lionel GUEZ <guez@lmd.ens.fr>
2021-09-15 17:01:33 +0200

    Polish

--- FindNetCDF.cmake
+++ FindNetCDF.cmake
@@ -7,18 +7,19 @@
 #[==[
 Provides the following variables:
 
-  * `NetCDF_FOUND`: Whether NetCDF was found or not.
-  * `NetCDF_INCLUDE_DIRS`: Include directories necessary to use NetCDF.
-  * `NetCDF_LIBRARIES`: Libraries necessary to use NetCDF.
-  * `NetCDF_VERSION`: The version of NetCDF found.
-  * `NetCDF::NetCDF`: A target to use with `target_link_libraries`.
-  * `NetCDF_HAS_PARALLEL`: Whether or not NetCDF was found with parallel IO support.
+* `NetCDF_FOUND`: Whether NetCDF was found or not.
+* `NetCDF_INCLUDE_DIRS`: Include directories necessary to use NetCDF.
+* `NetCDF_LIBRARIES`: Libraries necessary to use NetCDF.
+* `NetCDF_VERSION`: The version of NetCDF found.
+* `NetCDF::NetCDF`: A target to use with `target_link_libraries`.
+* `NetCDF_HAS_PARALLEL`: Whether or not NetCDF was found with parallel IO support.
 #]==]
 
 function(FindNetCDF_get_is_parallel_aware include_dir)
   file(STRINGS "${include_dir}/netcdf_meta.h" _netcdf_lines
     REGEX "#define[ \t]+NC_HAS_PARALLEL[ \t]")
-  string(REGEX REPLACE ".*NC_HAS_PARALLEL[ \t]*([0-1]+).*" "\\1" _netcdf_has_parallel "${_netcdf_lines}")
+  string(REGEX REPLACE ".*NC_HAS_PARALLEL[ \t]*([0-1]+).*" "\\1"
+    _netcdf_has_parallel "${_netcdf_lines}")
   if (_netcdf_has_parallel)
     set(NetCDF_HAS_PARALLEL TRUE PARENT_SCOPE)
   else()
@@ -28,6 +29,7 @@ endfunction()
 
 # Try to find a CMake-built NetCDF.
 find_package(netCDF CONFIG QUIET)
+
 if (netCDF_FOUND)
   # Forward the variables in a consistent way.
   set(NetCDF_FOUND "${netCDF_FOUND}")
@@ -56,9 +58,9 @@ if (netCDF_FOUND)
   endif ()
 
   FindNetCDF_get_is_parallel_aware("${NetCDF_INCLUDE_DIRS}")
-else ()
-
+else()
   find_package(PkgConfig QUIET)
+
   if (PkgConfig_FOUND)
     pkg_check_modules(_NetCDF QUIET netcdf IMPORTED_TARGET)
     if (_NetCDF_FOUND)
@@ -77,11 +79,11 @@ else ()
       include(FindPackageHandleStandardArgs)
       find_package_handle_standard_args(NetCDF
 	REQUIRED_VARS NetCDF_LIBRARIES
-	# This is not required because system-default include paths are not
-	# reported by `FindPkgConfig`, so this might be empty. Assume that if we
-	# have a library, the include directories are fine (if any) since
-	# PkgConfig reported that the package was found.
-	# NetCDF_INCLUDE_DIRS
+	# This is not required because system-default include paths
+	# are not reported by `FindPkgConfig`, so this might be
+	# empty. Assume that if we have a library, the include
+	# directories are fine (if any) since PkgConfig reported that
+	# the package was found.  NetCDF_INCLUDE_DIRS
 	VERSION_VAR NetCDF_VERSION)
 
       if (NOT TARGET NetCDF::NetCDF)
@@ -109,11 +111,16 @@ else ()
       unset(netCDF_DIR CACHE)
       file(STRINGS "${NetCDF_INCLUDE_DIR}/netcdf_meta.h" _netcdf_version_lines
 	REGEX "#define[ \t]+NC_VERSION_(MAJOR|MINOR|PATCH|NOTE)")
-      string(REGEX REPLACE ".*NC_VERSION_MAJOR *\([0-9]*\).*" "\\1" _netcdf_version_major "${_netcdf_version_lines}")
-      string(REGEX REPLACE ".*NC_VERSION_MINOR *\([0-9]*\).*" "\\1" _netcdf_version_minor "${_netcdf_version_lines}")
-      string(REGEX REPLACE ".*NC_VERSION_PATCH *\([0-9]*\).*" "\\1" _netcdf_version_patch "${_netcdf_version_lines}")
-      string(REGEX REPLACE ".*NC_VERSION_NOTE *\"\([^\"]*\)\".*" "\\1" _netcdf_version_note "${_netcdf_version_lines}")
-      set(NetCDF_VERSION "${_netcdf_version_major}.${_netcdf_version_minor}.${_netcdf_version_patch}${_netcdf_version_note}")
+      string(REGEX REPLACE ".*NC_VERSION_MAJOR *\([0-9]*\).*" "\\1"
+	_netcdf_version_major "${_netcdf_version_lines}")
+      string(REGEX REPLACE ".*NC_VERSION_MINOR *\([0-9]*\).*" "\\1"
+	_netcdf_version_minor "${_netcdf_version_lines}")
+      string(REGEX REPLACE ".*NC_VERSION_PATCH *\([0-9]*\).*" "\\1"
+	_netcdf_version_patch "${_netcdf_version_lines}")
+      string(REGEX REPLACE ".*NC_VERSION_NOTE *\"\([^\"]*\)\".*" "\\1"
+	_netcdf_version_note "${_netcdf_version_lines}")
+      set(NetCDF_VERSION
+	"${_netcdf_version_major}.${_netcdf_version_minor}.${_netcdf_version_patch}${_netcdf_version_note}")
       unset(_netcdf_version_major)
       unset(_netcdf_version_minor)
       unset(_netcdf_version_patch)
@@ -140,4 +147,4 @@ else ()
       endif ()
     endif ()
   endif()
-endif()
+endif ()
