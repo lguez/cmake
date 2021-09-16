lguez/cmake

commit cde6ae3938e7d85029d1cf0f370eb842ce180069

Do not create target `NetCDF_Fortran`
I do not see the point of the additional target `NetCDF_Fortran`: we
have the target `PkgConfig::NetCDF_Fortran` and we can just create an
alias `NetCDF_Fortran::NetCDF_Fortran` for
`PkgConfig::NetCDF_Fortran`.

FindNetCDF_Fortran.cmake
@@ -27,11 +27,5 @@ if (NOT TARGET NetCDF_Fortran::NetCDF_Fortran)
       "-- pkg_netcdf_fortran_includedir: ${pkg_netcdf_fortran_includedir}")
   endif()
 
-  add_library(NetCDF_Fortran INTERFACE IMPORTED)
-
-  set_target_properties(NetCDF_Fortran PROPERTIES
-    INTERFACE_LINK_LIBRARIES PkgConfig::NetCDF_Fortran)
-
-  target_link_libraries(NetCDF_Fortran INTERFACE NetCDF::NetCDF)
-  add_library(NetCDF_Fortran::NetCDF_Fortran ALIAS NetCDF_Fortran)
+  add_library(NetCDF_Fortran::NetCDF_Fortran ALIAS PkgConfig::NetCDF_Fortran)
 endif()

commit 1e3dc4de60ca6dacd5406d046d6f381812b34a4c

Bug fix: add dependency to NetCDF
Consumers linking to NetCDF_Fortran should also link to NetCDF. Bug
introduced in commit cde6ae3.

FindNetCDF_Fortran.cmake
@@ -31,6 +31,7 @@ if (NOT TARGET NetCDF_Fortran::NetCDF_Fortran)
       "pkg_netcdf_fortran_includedir: ${pkg_netcdf_fortran_includedir}")
   endif()
 
+  target_link_libraries(PkgConfig::NetCDF_Fortran INTERFACE NetCDF::NetCDF)
   add_library(NetCDF_Fortran::NetCDF_Fortran ALIAS PkgConfig::NetCDF_Fortran)
 endif()
 