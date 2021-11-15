837c45f2f804bc642c2c7be65a3e17dbc041b74e
Lionel GUEZ <guez@lmd.ens.fr>
2021-11-15 22:34:26 +0100

    Move files out of repository
    Because we are considering using this repository as a Git
    submodule. The toolchain files would not be at the right place in the
    subdirectory of a source directory.

--- nagfor.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_Fortran_FLAGS_INIT "-f2003")
-
-set(CMAKE_Fortran_FLAGS_DEBUG_INIT "-C=all -g90 -gline -nan -strict95")
-#  -C=undefined is not binary compatible with Fortran code compiled
-#  without that option, and is not compatible with calling C code
-#  via a BIND(C) interface.
