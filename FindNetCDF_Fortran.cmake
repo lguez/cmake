2ae607072751decef8d1eeb41d944e00fd819dd1
Lionel GUEZ <guez@lmd.ens.fr>
2021-12-15 21:59:42 +0100

    Polish

--- FindNetCDF_Fortran.cmake
+++ FindNetCDF_Fortran.cmake
@@ -22,6 +22,7 @@ else()
     find_package(netCDF CONFIG ${maybe_quiet})
 
     if(NOT netCDF_FOUND)
+      message("Trying with netCDF module...")
       find_package(netCDF ${maybe_quiet} ${maybe_required})
     endif()
   endif()
