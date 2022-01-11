f8652401500eb468bfa6abd52d5ee334de1627af
Lionel GUEZ <guez@lmd.ens.fr>
2022-01-11 21:15:56 +0100

    Polish

--- FindNetCDF_Fortran.cmake
+++ FindNetCDF_Fortran.cmake
@@ -31,16 +31,12 @@ else()
 
   if(netCDF_FOUND)
     find_package(PkgConfig REQUIRED)
-
     pkg_check_modules(netcdff REQUIRED IMPORTED_TARGET GLOBAL
       netcdf-fortran)
-
     pkg_get_variable(netcdf_fortran_pcfiledir netcdf-fortran pcfiledir)
     message(DEBUG "Location of .pc file: ${netcdf_fortran_pcfiledir}")
-
     message(VERBOSE
       "NetCDF-Fortran include directories: ${netcdff_INCLUDE_DIRS}")
-
     message(VERBOSE "NetCDF-Fortran libraries: ${netcdff_LINK_LIBRARIES}")
 
     if(PKG_CONFIG_VERSION_STRING VERSION_LESS "0.29.2"
@@ -52,10 +48,8 @@ else()
       # pkg_check_modules.
       # Version > 1.1 means we are using pkgconf from pkgconf.org
       pkg_get_variable(pkg_netcdf_fortran_includedir netcdf-fortran includedir)
-
       target_include_directories(PkgConfig::netcdff INTERFACE
 	${pkg_netcdf_fortran_includedir})
-
       message(DEBUG
 	"pkg_netcdf_fortran_includedir: ${pkg_netcdf_fortran_includedir}")
     endif()
