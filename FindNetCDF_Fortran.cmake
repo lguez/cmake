0589e9b73c4e412b0b8c17c43899104fac4a5437
Lionel GUEZ <guez@lmd.ens.fr>
2022-01-14 19:24:07 +0100

    Do not find NetCDF
    Revert commit 4a6266e9. Change of philosophy. This was not
    consistent. If NetCDF-Fortran were responsible for finding NetCDF then
    NetCDF would be responsible for finding HDF5. Then we would have to
    include a find HDF5 module. And so on. We have to rely on the
    information given by pkg-config to set the correct compilation and
    linking flags.

--- FindNetCDF_Fortran.cmake
+++ FindNetCDF_Fortran.cmake
@@ -3,33 +3,6 @@ list(APPEND CMAKE_MESSAGE_CONTEXT FindNetCDF_Fortran)
 if(TARGET NetCDF_Fortran::netcdff)
   set(NetCDF_Fortran_FOUND True)
 else()
-  # Find NetCDF dependency:
-
-  option(FIND_PACKAGE_PREFER_MODULE_netCDF
-    "Use directly the find module for NetCDF")
-
-  if(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
-    set(maybe_quiet QUIET)
-  endif()
-  
-  if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
-    set(maybe_required REQUIRED)
-  endif()
-  
-  if(FIND_PACKAGE_PREFER_MODULE_netCDF)
-    find_package(netCDF ${maybe_quiet} ${maybe_required})
-  else()
-    find_package(netCDF CONFIG ${maybe_quiet})
-
-    if(NOT netCDF_FOUND)
-      message(STATUS "Trying with netCDF module...")
-      find_package(netCDF ${maybe_quiet} ${maybe_required})
-    endif()
-  endif()
-
-  #-
-
-  if(netCDF_FOUND)
     find_package(PkgConfig REQUIRED)
     message(DEBUG "PKG_CONFIG_EXECUTABLE: ${PKG_CONFIG_EXECUTABLE}")
     pkg_check_modules(netcdff REQUIRED IMPORTED_TARGET GLOBAL
@@ -52,12 +25,8 @@ else()
 	${pkg_nf_includedir})
     endif()
 
-    target_link_libraries(PkgConfig::netcdff INTERFACE netCDF::netcdf)
     add_library(NetCDF_Fortran::netcdff ALIAS PkgConfig::netcdff)
     set(NetCDF_Fortran_FOUND True)
-  else()
-    set(NetCDF_Fortran_FOUND False)
-  endif()
 endif()
 
 list(POP_BACK CMAKE_MESSAGE_CONTEXT)
