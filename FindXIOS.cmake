e4a85ba22cc8cc701b419de86eb7412878f4c2a2
Lionel GUEZ <guez@lmd.ens.fr>
2022-02-08 17:58:46 +0100

    Polish

--- FindXIOS.cmake
+++ FindXIOS.cmake
@@ -1,19 +1,18 @@
+list(APPEND CMAKE_MESSAGE_CONTEXT FindXIOS)
+
 find_path(XIOS_INCLUDE_DIR xios.mod)
 find_library(XIOS_LIBRARY xios)
 include(FindPackageHandleStandardArgs)
-
 find_package_handle_standard_args(XIOS REQUIRED_VARS XIOS_INCLUDE_DIR
   XIOS_LIBRARY)
 
 if (XIOS_FOUND)
   add_library(XIOS::xios STATIC IMPORTED)
-
   set_target_properties(XIOS::xios PROPERTIES IMPORTED_LOCATION
     ${XIOS_LIBRARY} IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
-  
   target_include_directories(XIOS::xios INTERFACE ${XIOS_INCLUDE_DIR})
-
   target_link_libraries(XIOS::xios INTERFACE MPI::MPI_CXX
     NetCDF_Fortran::netcdff)
-
 endif()
+
+list(POP_BACK CMAKE_MESSAGE_CONTEXT)
